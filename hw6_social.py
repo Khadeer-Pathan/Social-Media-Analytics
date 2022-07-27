5fc7e9a1c6753e46788b70219b71ec9fffa83bf8
Khadeer-Pathan <pathankhadeer@msitprogram.net>
2022-07-27 17:31:54 +0530

    Implementation of makeDataFrame() is done

--- hw6_social.py
+++ hw6_social.py
@@ -25,7 +25,8 @@ Parameters: str
 Returns: dataframe
 '''
 def makeDataFrame(filename):
-    return
+    df = pd.read_csv(filename)
+    return df
 
 
 '''
@@ -262,6 +263,7 @@ def scatterPlot(xValues, yValues, labels, title):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
+    test.testMakeDataFrame()
     """ print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
     test.week1Tests()
     print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
